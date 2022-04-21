406f3c0aeffeb27d4f5e50403c00b6e608b95dfa
Jerome <collognat@osimatic.com>
2022-04-21 10:50:44 +0200

    module php.min

--- index.js
+++ index.js
@@ -1,33 +1,34 @@
 //A noter : les réécritures de prototypes définies dans certains fichiers n'ont pas besoin d'être exportés depuis le module en question (exemple Array / Object dans array.js).
 //Il faut uniquement appeler require() (sans récupérer le retour) et ajouter la/les classe(s) concernée(s) dans l'export ci-dessous (l'appel de require() applique l'extension).
 
-const { HTTPRequest, Cookie, UrlAndQueryString } = require('./network.js');
-const { IBAN } = require('./bank.js');
-const { AudioMedia } = require('./media.js');
-const { PersonName, Email, TelephoneNumber } = require('./contact_details.js');
-const { CountDown } = require('./count_down.js');
-const { DataTable } = require('./data_table.js');
-const { DateTime, TimestampUnix, SqlDate, SqlTime, SqlDateTime, InputPeriod } = require('./date_time.js');
-const { DetailsSubArray } = require('./details_sub_array.js');
-const { Duration } = require('./duration.js');
-const { File, CSV, Img } = require('./file.js');
-const { FlashMessage } = require('./flash_message.js');
-const { FormHelper } = require('./form_helper.js');
-const { GoogleMap } = require('./google_maps.js');
-const { ImportFromCsv } = require('./import_from_csv.js');
-const { JwtToken, JwtSession } = require('./jwt.js');
-const { ListBox } = require('./list_box.js');
-const { COUNTRIES_LIST, Country, PostalAddress, Location } = require('./location.js');
-const { SocialNetwork } = require('./social_network.js');
+const { HTTPRequest, Cookie, UrlAndQueryString } = require('./network');
+const { IBAN } = require('./bank');
+const { AudioMedia } = require('./media');
+const { PersonName, Email, TelephoneNumber } = require('./contact_details');
+const { CountDown } = require('./count_down');
+const { DataTable } = require('./data_table');
+const { DateTime, TimestampUnix, SqlDate, SqlTime, SqlDateTime, InputPeriod } = require('./date_time');
+const { DetailsSubArray } = require('./details_sub_array');
+const { Duration } = require('./duration');
+const { File, CSV, Img } = require('./file');
+const { FlashMessage } = require('./flash_message');
+const { FormHelper } = require('./form_helper');
+const { GoogleMap } = require('./google_maps');
+const { ImportFromCsv } = require('./import_from_csv');
+const { JwtToken, JwtSession } = require('./jwt');
+const { ListBox } = require('./list_box');
+const { COUNTRIES_LIST, Country, PostalAddress, Location } = require('./location');
+const { SocialNetwork } = require('./social_network');
 const { getValuesByKeyInArrayOfArrays, renameKeys, renameKeysByCallback } = require('./array');
-const { NumberValue } = require('./number.js');
-const { selectionnerContenuNode, copierTexte } = require('./string.js');
-const { addBookmark, sleep, refresh } = require('./util.js');
+const { NumberValue } = require('./number');
+const { selectionnerContenuNode, copierTexte } = require('./string');
+const { addBookmark, sleep, refresh } = require('./util');
+const { chr, ord, trim, empty } = require('./php.min');
 
 module.exports = {
     HTTPRequest, Cookie, UrlAndQueryString, IBAN, AudioMedia, PersonName, Email, TelephoneNumber, CountDown, DataTable,
     DateTime, TimestampUnix, SqlDate, SqlTime, SqlDateTime, InputPeriod, DetailsSubArray, Duration, File, CSV, Img,
     FlashMessage, FormHelper, GoogleMap, ImportFromCsv, JwtToken, JwtSession, ListBox, COUNTRIES_LIST, Country, PostalAddress, 
     Location, SocialNetwork, Array, Object, getValuesByKeyInArrayOfArrays, renameKeys, renameKeysByCallback, Number, NumberValue,
-    String, selectionnerContenuNode, copierTexte, addBookmark, sleep, refresh
+    String, selectionnerContenuNode, copierTexte, addBookmark, sleep, refresh, chr, ord, trim, empty
 };
