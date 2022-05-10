3f235a44bb91a5c612e213c587610b718e713bed
Jerome <collognat@osimatic.com>
2022-05-10 16:21:07 +0200

    corrections classe WebSocket

--- web_socket.js
+++ web_socket.js
@@ -5,62 +5,56 @@ class WebSocket {
      * customBusEvents : events "spéciaux" en provenance de l'event bus qui seront dispatchés vers la socket : array({ 'custom_name' => 'event_name' }, ...)
     */
     static setEventListeners(socketEvents, busEvents, customBusEvents) {
+        const { EventBus } = require('./event_bus');
+
+        this.eventBus = new EventBus();
         this.socketEvents = socketEvents;
 		this.busEvents = busEvents;
         this.customBusEvents = customBusEvents;
     }
 
-    static getEventBus() {
-        return this.eventBus;
-    }
-
-	static connect(url, options, connectInitPayload, onConnectionAckCallback, onUnavailableCallback) {
+	static connect(url, options, connectInitPayload, onUnavailableCallback, onConnectionAckCallback) {
         const io = require('socket.io-client');
-        const EventBus = require('./event_bus');
-        
-        this.eventBus = new EventBus();
+    
         this.logged = false;
 		this.instance = io(url, options);
 
-		this.initEventsListeners();
+		this.initEventListeners();
 		this.initConnectionListeners(onUnavailableCallback);
 
 		this.instance.on('connect', () => {
 			if (this.logged) {
-				this.clear(onUnavailableCallback);
+				this.clear();
 			}
 
 			this.logged = true;
-            this.instance.emit('connect_init', connectInitPayload, onConnectionAckCallback);
+
+			this.instance.emit('connect_init', connectInitPayload, onConnectionAckCallback);
 		});
 	}
 
 	static initConnectionListeners(onUnavailableCallback) {
-		['connect_error', 'connect_timeout', 'reconnect_error', 'reconnect_failed', 'disconnect'].forEach(event => this.instance.on(event, () => this.clear(event, onUnavailableCallback)));
-		this.instance.on('error', (error) => this.clear(error, onUnavailableCallback));
+		['connect_error', 'connect_timeout', 'reconnect_error', 'reconnect_failed', 'disconnect'].forEach(event => this.instance.on(event, () => this.clear(onUnavailableCallback, event)));
+		this.instance.on('error', (error) => this.clear(onUnavailableCallback, error));
 	}
 
-	static clear(error, onUnavailableCallback) {
-		this.unregisterOutcomingBusEvents();
-        
-        if (onUnavailableCallback != 'undefined') {
+	static clear(onUnavailableCallback, error) {
+		this.unregisterEventListener();
+
+		if (typeof onUnavailableCallback !== 'undefined') {
             onUnavailableCallback(error);
         }
 	}
 
-    static unregisterIncomingBusEvents() {
-        
-    }
-
-	static unregisterOutcomingBusEvents() {
-		this.busEvents.forEach(event => this.eventBus.unsubscribe(event, {}));
-        this.customBusEvents.forEach((object) => this.eventBus.unsubscribe(object.custom_name, {}));
-	}
-
-	static initEventsListeners() {
+    static initEventListeners() {
 		this.socketEvents.forEach(event => this.instance.on(event, (payload) => this.eventBus.publish(event, payload)));
 		this.busEvents.forEach(event => this.eventBus.subscribe(event, (payload) => this.instance.emit(event, payload)));
         this.customBusEvents.forEach((object) => this.eventBus.subscribe(object.custom_name, (payload) => this.instance.emit(object.event_name, payload)));
+	}
+
+	static unregisterEventListener() {
+		this.busEvents.forEach(event => this.eventBus.unsubscribe(event, {}));
+        this.customBusEvents.forEach((object) => this.eventBus.unsubscribe(object.custom_name, {}));
 	}
 }
 
