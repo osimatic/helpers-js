72d7f84ea542adfebce603d2efff23f06b7126c2
Benoit <guiraudou@osimatic.com>
2024-07-31 09:22:41 +0200

    add id param

--- flash_message.js
+++ flash_message.js
@@ -1,21 +1,21 @@
 class FlashMessage {
-	static displaySuccess(message, reload=false, modal=null, onMessageHidden=null) {
-		this.display('success', message, reload, modal, onMessageHidden);
+	static displaySuccess(message, reload=false, modal=null, onMessageHidden=null, domId=null) {
+		this.display('success', message, reload, modal, onMessageHidden, domId);
 	}
-	static displayWarning(message, modal=null, onMessageHidden=null) {
-		this.display('warning', message, false, modal, onMessageHidden);
+	static displayWarning(message, modal=null, onMessageHidden=null, domId=null) {
+		this.display('warning', message, false, modal, onMessageHidden, domId);
 	}
-	static displayError(message, modal=null, onMessageHidden=null) {
-		this.display('danger', message, false, modal, onMessageHidden);
+	static displayError(message, modal=null, onMessageHidden=null, domId=null) {
+		this.display('danger', message, false, modal, onMessageHidden, domId);
 	}
 
-	static display(type, message, reload=false, modal=null, onMessageHidden=null) {
+	static display(type, message, reload=false, modal=null, onMessageHidden=null, domId=null) {
 		if (null !== modal) {
 			modal.modal('hide');
 		}
 
 		$('div.snackbar').remove();
-		let snackbar = $('<div class="snackbar '+type+'"></div>');
+		let snackbar = $('<div class="snackbar '+type+'" '+(null !== domId ? 'id="'+domId+'"' : '')+'></div>');
 		$('html body').append(snackbar);
 		snackbar.html(message);
 		snackbar.addClass('show');
